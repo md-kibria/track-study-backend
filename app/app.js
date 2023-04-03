a5e7dafc528267e14d8bb15ac33ab563ef9d3db7
Md Kibria <mdkibria684@gmail.com>
2023-04-03 12:23:05 +0600

    using cors func instant of vercel configuration

--- app/app.js
+++ app/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const middlewares = require("../middlewares");
 const routes = require("../routes");
+const cors = require("cor")
 
 const app = express();
 dotenv.config();
 
+app.use(cors())
 app.use(express.static("public"));
 app.use(middlewares);
 app.use(routes);
