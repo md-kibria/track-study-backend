f1bc48d21f76d03b787d95110b0d5d62c329d361
Md Kibria <mdkibria684@gmail.com>
2023-04-06 13:11:05 +0600

    Add a /test route

--- app/app.js
+++ app/app.js
@@ -6,6 +6,12 @@ const routes = require("../routes");
 const app = express();
 dotenv.config();
 
+app.get('/test', (req, res) => {
+    res.status(200).json({
+        msg: 'This is test'
+    })
+})
+
 app.use(express.static("public"));
 app.use(middlewares);
 app.use(routes);
