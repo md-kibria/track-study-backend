e110ab474ea5f3629cded00248c6defa07f30e71
Md Kibria <mdkibria684@gmail.com>
2023-04-06 12:31:35 +0600

    Remove the custom middleware for CORS

--- app/app.js
+++ app/app.js
@@ -6,20 +6,6 @@ const routes = require("../routes");
 const app = express();
 dotenv.config();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    console.log('CORS middleware');
-    next()
-})
-
 app.use(express.static("public"));
 app.use(middlewares);
 app.use(routes);
