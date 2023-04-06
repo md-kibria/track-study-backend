70ab161a9caace3c92fb29687416f969ef140fb0
Md Kibria <mdkibria684@gmail.com>
2023-04-06 13:30:04 +0600

    add mongoose strictQuery = false

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const DB_URL = process.env.DB_URL || "mongodb://127.0.0.1:27017/track-study";
 
 const server = http.createServer(app);
 
-// mongoose.set("strictQuery", false);
+mongoose.set("strictQuery", false);
 mongoose.connect(DB_URL, (err, data) => {
   if (err) {
     console.log(err);
