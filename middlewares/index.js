752840ee7b40d7fb959e511fedf44f55288c3294
Md Kibria <78228935+md-kibria@users.noreply.github.com>
2024-02-27 12:24:21 +0600

    Cors middleware code

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,10 +3,11 @@ const express = require("express");
 const cors = require("cors");
 
 const middlewares = [
-    cors({
-        origin: "https://track-study-frontend.vercel.app/",
-        methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE']
-    }),
+   // cors({
+    //     origin: "https://track-study-frontend.vercel.app/",
+    //     methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE']
+    // }),
+    cors(),
     morgan("dev"),
     express.json()
 ];
